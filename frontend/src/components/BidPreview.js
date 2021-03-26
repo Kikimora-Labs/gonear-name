2903bb8df69870054556056fa7448477f681488d
Alex Kouprin <kpr@nearprotocol.com>
2021-03-26 11:46:29 +0000

    mobile minor adoption

--- frontend/src/components/BidPreview.js
+++ frontend/src/components/BidPreview.js
@@ -16,16 +16,17 @@ function BidPreview (props) {
 
   const { data: bid } = useSWR(['bid_id', bidId], fetchBid, { errorRetryInterval: 100 })
   const textBetsNum = bid && bid.bets && bid.bets.length > 2 ? 'bets' : 'bet'
+  const buttonWidthPx = window.innerWidth < 800 ? '120px' : '300px'
+
   return bid ? (
     <div className='pt-3'>
-
       <div className='d-flex flex-row w-100 align-items-center' style={{ maxWidth: '640px' }}>
         <div className='d-flex flex-row justify-content-between w-100'>
           <Link to={`/bid/${bidId}`} style={{ textDecoration: 'none', color: 'white' }}>{bidId}</Link>
           {bid.claimedBy ? (<small className='text-end ps-1 my-gray'>claimed&nbsp;by <Link className='navigate' to={`/profile/${bid.claimedBy}`}>{bid.claimedBy}</Link></small>
-          ) : (bid.bets && bid.bets.length > 1 && <small className='my-gray'>{bid.bets.length - 1} {textBetsNum}</small>)}
+          ) : (bid.bets && bid.bets.length > 1 && <small className='text-end ps-1 my-gray'>{bid.bets.length - 1}&nbsp;{textBetsNum}</small>)}
         </div>
-        <div className='ps-2' style={{ minWidth: '200px', maxWidth: '300px' }}>
+        <div className='ps-2' style={{ width: buttonWidthPx }}>
           {!bid.isAtMarket ? (<AcquireButton {...props} bidId={bidId} />) : (bid.isOnAcquisition
             ? (<DetailsButton {...props} bidId={bidId} />)
             : (<PriceButton {...props} bidId={bidId} price={bid.betPrice} forfeit={bid.forfeit} />))}
